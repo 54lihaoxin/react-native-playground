263512f2f11a5d731c91bbdbf5be2f936e01dc17
Haoxin Li <54lihaoxin@gmail.com>
2018-03-31 09:29:52 -0700

    Add stub view controller to get ready for some RN code

---
+++ ios/ReactNativePlayground/RNHighScoreViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RNHighScoreViewController.swift
+//  ReactNativePlayground
+//
+//  Created by Li, Haoxin on 3/24/18.
+//  Copyright © 2018 Haoxin. All rights reserved.
+//
+
+import UIKit
+
+class RNHighScoreViewController: UIViewController {
+
+    @IBAction func showHighScore(_ sender: Any) {
+        // TODO:
+    }
+}
