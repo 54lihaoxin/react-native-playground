c13120584cd325036e2e87e0bf3123a9df0768d2
Haoxin Li <54lihaoxin@gmail.com>
2018-04-02 13:51:56 -0700

    Set native map view display area from JavaScript.

--- ios/ReactNativePlayground/ReactExternalModuleBridge.m
+++ ios/ReactNativePlayground/ReactExternalModuleBridge.m
@@ -6,8 +6,44 @@
 //  Copyright © 2018 Haoxin. All rights reserved.
 //
 
+#import <MapKit/MapKit.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTConvert.h>
 #import <React/RCTViewManager.h>
+
+
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import <React/RCTConvert.h>
+#import <React/RCTConvert+CoreLocation.h>
+
+@interface RCTConvert(Mapkit)
+
++ (MKCoordinateSpan)MKCoordinateSpan:(id)json;
++ (MKCoordinateRegion)MKCoordinateRegion:(id)json;
+
+@end
+
+@implementation RCTConvert(MapKit)
+
++ (MKCoordinateSpan)MKCoordinateSpan:(id)json
+{
+    json = [self NSDictionary:json];
+    return (MKCoordinateSpan){
+        [self CLLocationDegrees:json[@"latitudeDelta"]],
+        [self CLLocationDegrees:json[@"longitudeDelta"]]
+    };
+}
+
++ (MKCoordinateRegion)MKCoordinateRegion:(id)json
+{
+    return (MKCoordinateRegion){
+        [self CLLocationCoordinate2D:json],
+        [self MKCoordinateSpan:json]
+    };
+}
+
+@end
 
 @interface RCT_EXTERN_MODULE(BasicBridgingExample, NSObject)
 
@@ -15,14 +51,18 @@ RCT_EXTERN_METHOD(addEvent:(NSString *)name
                   location:(NSString *)location
                   numberOfSecondsSince1970:(nonnull NSNumber *)numberOfSecondsSince1970
                   details:(NSDictionary *)details
-                  callback:(RCTResponseSenderBlock)callback);
+                  callback:(RCTResponseSenderBlock)callback)
 
 @end
 
 @interface RCT_EXTERN_MODULE(RNTMapManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(zoomEnabled, BOOL)
+RCT_CUSTOM_VIEW_PROPERTY(region, MKCoordinateRegion, MKMapView)
+{
+    [view setRegion:(json ? [RCTConvert MKCoordinateRegion:json] : defaultView.region) animated:YES];
+}
 
-RCT_EXTERN_METHOD(view);
+RCT_EXTERN_METHOD(view)
 
 @end
--- javascript/RNNativeMapView.js
+++ javascript/RNNativeMapView.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import NativeMapView from './NativeUiComponent/NativeMapView.ios'
+import PropTypes from 'prop-types';
 import { AppRegistry } from 'react-native';
+
+// See original tutorial here: https://facebook.github.io/react-native/docs/native-components-ios.html
 
 class RNNativeMapView extends React.Component {
   render() {
-    return <NativeMapView style={{ flex: 1 }} zoomEnabled={false}/>;
+    var region = {
+      latitude: 37.48,
+      longitude: -122.16,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    };
+    return (
+      <NativeMapView
+        region={region}
+        zoomEnabled={false}
+        style={{ flex: 1 }}
+      />
+    );
   }
 }
+
+NativeMapView.propTypes = {
+  /**
+   * A Boolean value that determines whether the user may use pinch
+   * gestures to zoom in and out of the map.
+   */
+  zoomEnabled: PropTypes.bool,
+
+  /**
+   * The region to be displayed by the map.
+   *
+   * The region is defined by the center coordinates and the span of
+   * coordinates to display.
+   */
+  region: PropTypes.shape({
+    /**
+     * Coordinates for the center of the map.
+     */
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+
+    /**
+     * Distance between the minimum and the maximum latitude/longitude
+     * to be displayed.
+     */
+    latitudeDelta: PropTypes.number.isRequired,
+    longitudeDelta: PropTypes.number.isRequired,
+  }),
+};
 
 // Module name
 AppRegistry.registerComponent('RNNativeMapView', () => RNNativeMapView);
