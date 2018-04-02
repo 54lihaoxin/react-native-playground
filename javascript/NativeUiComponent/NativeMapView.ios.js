2e568523be0689b04758064b5f46ea24aeeac7d5
Haoxin Li <54lihaoxin@gmail.com>
2018-04-02 14:02:02 -0700

    Reorganize native map view code

--- javascript/NativeUiComponent/NativeMapView.ios.js
+++ javascript/NativeUiComponent/NativeMapView.ios.js
@@ -1,6 +1,45 @@
 // import native iOS map view
 
+import React from 'react';
+import PropTypes from 'prop-types';
 import { requireNativeComponent } from 'react-native';
 
+// See original tutorial here: https://facebook.github.io/react-native/docs/native-components-ios.html
+
+class NativeMapView extends React.Component {
+  render() {
+    return <RNTMap {...this.props} />;
+  }
+}
+
+NativeMapView.propTypes = {
+  /**
+   * A Boolean value that determines whether the user may use pinch
+   * gestures to zoom in and out of the map.
+   */
+  zoomEnabled: PropTypes.bool,
+
+  /**
+   * The region to be displayed by the map.
+   *
+   * The region is defined by the center coordinates and the span of
+   * coordinates to display.
+   */
+  region: PropTypes.shape({
+    /**
+     * Coordinates for the center of the map.
+     */
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+
+    /**
+     * Distance between the minimum and the maximum latitude/longitude
+     * to be displayed.
+     */
+    latitudeDelta: PropTypes.number.isRequired,
+    longitudeDelta: PropTypes.number.isRequired,
+  }),
+};
+
 // requireNativeComponent automatically resolves 'RNTMap' to 'RNTMapManager'
-module.exports = requireNativeComponent('RNTMap', null);
+module.exports = requireNativeComponent('RNTMap', NativeMapView);
--- javascript/RNNativeMapView.js
+++ javascript/RNNativeMapView.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import NativeMapView from './NativeUiComponent/NativeMapView.ios'
-import PropTypes from 'prop-types';
 import { AppRegistry } from 'react-native';
 
 // See original tutorial here: https://facebook.github.io/react-native/docs/native-components-ios.html
@@ -22,35 +21,6 @@ class RNNativeMapView extends React.Component {
     );
   }
 }
-
-NativeMapView.propTypes = {
-  /**
-   * A Boolean value that determines whether the user may use pinch
-   * gestures to zoom in and out of the map.
-   */
-  zoomEnabled: PropTypes.bool,
-
-  /**
-   * The region to be displayed by the map.
-   *
-   * The region is defined by the center coordinates and the span of
-   * coordinates to display.
-   */
-  region: PropTypes.shape({
-    /**
-     * Coordinates for the center of the map.
-     */
-    latitude: PropTypes.number.isRequired,
-    longitude: PropTypes.number.isRequired,
-
-    /**
-     * Distance between the minimum and the maximum latitude/longitude
-     * to be displayed.
-     */
-    latitudeDelta: PropTypes.number.isRequired,
-    longitudeDelta: PropTypes.number.isRequired,
-  }),
-};
 
 // Module name
 AppRegistry.registerComponent('RNNativeMapView', () => RNNativeMapView);
